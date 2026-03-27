8f5304304165044d880a45433fa87cfa6e8d6184
Alex Seong <alex.y.seong@gmail.com>
2026-03-27 12:47:01 -0400

    src/sqe

---
+++ src/sqe/markov.py
@@ -0,0 +1,14 @@
+import numpy as np
+from typing import Union, List
+
+class QuantumMarkovModel:
+    """
+    Classical-to-Quantum Markov Transition Engine.
+    Translate classical stochastic processes into quantum density matrix evolutions.
+    """
+
+    def __init__(self, states_count: int):
+        pass
+
+if __name__ == "__main__":
+    qmn = QuantumMarkovModel(states_count=3)
